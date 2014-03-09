bae7f555d7ef9b733e1cf518c0248745524e9065
Sergei Ianovich <ynvich@gmail.com>
2014-03-09 19:23:39 +0400

    configure available locales (closes #4)
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- test/i18n.js
+++ test/i18n.js
@@ -12,4 +12,29 @@ describe('i18n', function () {
     i18n.t.should.be.type('function');
     i18n.t('hi').should.equal('Greetings!');
   });
+
+  it('should accept option for locales directory', function () {
+    var i18n = require('../lib/i18n')({ locales_directory: 'a/b' });
+
+    i18n.t.should.be.type('function');
+    i18n.t('hi').should.equal('Hi!');
+  });
+
+  describe('with multiple languages', function () {
+    var i18n;
+    before (function () {
+      i18n = require('../lib/i18n')({ supported_languages: ['ru', 'en'] });
+    })
+
+    it('should accept option for supported languages', function () {
+      i18n.t('hi').should.equal('Privet!');
+    });
+
+    it('should accept all supported languages', function () {
+      i18n.setLanguage('en');
+      i18n.t('hi').should.equal('Greetings!');
+    });
+  });
 })
+
+// vim:ts=2 sts=2 sw=2 et:
