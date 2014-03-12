ec5db1db566624acc214ac789d149ea31c5dfe34
Sergei Ianovich <ynvich@gmail.com>
2014-03-12 20:02:41 +0400

    import Accept-Language test suite from i18n-abide
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- test/accept-lang.js
+++ test/accept-lang.js
@@ -16,9 +16,44 @@ var acceptLang = require('../lib/accept-lang');
 describe('i18n best language', function () {
   it('should find exact language match', function () {
     var accept = 'pa,sv;q=0.8,fi;q=0.7,it-ch;q=0.5,en-us;q=0.3,en;q=0.2';
+    var supported = ['en-US', 'af', 'pa'];
+
+    acceptLang(accept, supported).should.be.equal('pa');
+  });
+
+  it("should find a matching locale even if region doen't match", function () {
+    var accept = 'pa-it,sv;q=0.8,fi;q=0.7,it-ch;q=0.5,en-us;q=0.3,en;q=0.2';
+    var supported = ['en-US', 'af', 'pa'];
+
+    acceptLang(accept, supported).should.be.equal('pa');
+  });
+
+  it("should only match region exactly", function () {
+    var accept = 'pa,sv;q=0.8,fi;q=0.7,it-ch;q=0.5,en-us;q=0.3,en;q=0.2';
     var supported = ['en-US', 'af', 'pa-IT'];
 
     acceptLang(accept, supported).should.be.equal('en-us');
+  });
+
+  it("should not match Finnish to Ligurian", function () {
+    var accept = 'fil-PH,fil;q=0.97,en-US;q=0.94,en;q=0.91';
+    var supported = ['en-US', 'fi'];
+
+    acceptLang(accept, supported).should.be.equal('en-US');
+  });
+
+  it("should support Ligurian", function () {
+    var accept = 'fil-PH,fil;q=0.97,en-US;q=0.94,en;q=0.91';
+    var supported = ['en-US', 'fi', 'fil-PH'];
+
+    acceptLang(accept, supported).should.be.equal('fil-PH');
+  });
+
+  it("should support Ligurian without a region", function () {
+    var accept = 'fil-PH,fil;q=0.97,en-US;q=0.94,en;q=0.91';
+    var supported = ['en-US', 'fi', 'fil'];
+
+    acceptLang(accept, supported).should.be.equal('fil');
   });
 })
 
